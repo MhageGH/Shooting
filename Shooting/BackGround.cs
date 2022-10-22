289e3b86c5945ac0aab1f5c21339f01e5024c9cd
Mhage <masahage@soleil.ocn.ne.jp>
2022-10-22 17:53:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Shooting/BackGround.cs
@@ -0,0 +1,40 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace Shooting
+{
+    internal class BackGround
+    {
+        Mat src_img = OpenCvSharp.Cv2.ImRead("Ground.png");
+        int shrink = 150;
+        int offset = 0;
+        int speed = 5;
+        int trimHeight = 500;
+        OpenCvSharp.Size screen_size = new OpenCvSharp.Size(500, 600);
+
+        public BackGround()
+        {
+            Cv2.VConcat(src_img, src_img, src_img);
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            offset = (offset + speed) % (src_img.Height / 2);
+            var src_img_trimmed = src_img.Clone(new Rect(0, src_img.Height - offset - trimHeight, src_img.Width, trimHeight));
+            var inPoints = new Point2f[] 
+            {
+                new Point2f(0, 0), new Point2f(src_img_trimmed.Width, 0), new Point2f(src_img_trimmed.Width, src_img_trimmed.Height), new Point2f(0, src_img_trimmed.Height) 
+            };
+            var outPoints = new Point2f[]
+            {
+                new Point2f(shrink, 0), new Point2f(src_img_trimmed.Width - shrink, 0), new Point2f(src_img_trimmed.Width, src_img_trimmed.Height), new Point2f(0, src_img_trimmed.Height)
+            };
+            var transMat = Cv2.GetPerspectiveTransform(inPoints, outPoints);
+            var dst_img = new Mat(new OpenCvSharp.Size(src_img_trimmed.Width, src_img_trimmed.Height), MatType.CV_8UC3);
+            Cv2.WarpPerspective(src_img_trimmed, dst_img, transMat, dst_img.Size());
+            dst_img = dst_img.Clone(new Rect(shrink, 0, src_img_trimmed.Width - 2 * shrink, src_img_trimmed.Height));
+            Cv2.Resize(dst_img, dst_img, screen_size);
+            graphics.DrawImage(dst_img.ToBitmap(), 0, 0);
+        }
+    }
+}
---
+++ Shooting/Form1.cs
@@ -0,0 +1,76 @@
+
+namespace Shooting
+{
+    public partial class Form1 : Form
+    {
+        int animationTime = 0;
+        BackGround backGround = new BackGround();
+        Image imageMinoriko = Image.FromFile("Minoriko.png");
+        Image imageBullet1 = Image.FromFile("Bullet1.png");
+        Image imageBullet2 = Image.FromFile("Bullet2.png");
+        List<Bullet> bullet1s = new List<Bullet>();
+        List<Bullet> bullet2s = new List<Bullet>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+            animationTime = (animationTime + 1) % 40;
+            if (animationTime % 5 == 0)
+            {
+                bullet1s.Add(new Bullet(new Point(100, 0), new Point(2, 10), imageBullet1));
+                bullet1s.Add(new Bullet(new Point(150, 0), new Point(1, 10), imageBullet1));
+                bullet1s.Add(new Bullet(new Point(200, 0), new Point(0, 10), imageBullet1));
+                bullet1s.Add(new Bullet(new Point(250, 0), new Point(-1, 10), imageBullet1));
+                bullet1s.Add(new Bullet(new Point(300, 0), new Point(-2, 10), imageBullet1));
+                bullet2s.Add(new Bullet(new Point(210, 0), new Point(4, 5), imageBullet2));
+                bullet2s.Add(new Bullet(new Point(265, 0), new Point(2, 5), imageBullet2));
+                bullet2s.Add(new Bullet(new Point(310, 0), new Point(0, 5), imageBullet2));
+                bullet2s.Add(new Bullet(new Point(360, 0), new Point(-2, 5), imageBullet2));
+                bullet2s.Add(new Bullet(new Point(410, 0), new Point(-4, 5), imageBullet2));
+            }
+            for (int i = 0; i < bullet1s.Count; i++)
+            {
+                bullet1s[i].position.X += bullet1s[i].speed.X;
+                bullet1s[i].position.Y += bullet1s[i].speed.Y;
+            }
+            for (int i = 0; i < bullet2s.Count; i++)
+            {
+                bullet2s[i].position.X += bullet2s[i].speed.X;
+                bullet2s[i].position.Y += bullet2s[i].speed.Y;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            backGround.Draw(e.Graphics);
+            int n = animationTime / 10;
+            e.Graphics.DrawImage(imageMinoriko, 220, 400, new Rectangle(50 * n, 0, 50, 50), GraphicsUnit.Pixel);
+
+            for (int i = 0; i < bullet1s.Count; i++)
+            {
+                //e.Graphics.DrawImage(bullet1s[i].image, bullet1s[i].position);
+            }
+            for (int i = 0; i < bullet2s.Count; i++)
+            {
+                //e.Graphics.DrawImage(bullet2s[i].image, bullet2s[i].position);
+            }
+        }
+    }
+
+    class Bullet
+    {
+        public Point position;
+        public Point speed;
+        public Image image;
+        public Bullet(Point position, Point speed, Image image)
+        {
+            this.position = position;
+            this.speed = speed;
+            this.image = image;
+        }
+    }
+}
