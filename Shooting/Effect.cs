c4ed79dc42d84b30d61e0d93bc5f6f8024fc09f7
Mhage <masahage@soleil.ocn.ne.jp>
2022-11-12 14:56:50 +0900

    エフェクトの追加

--- Shooting/Effect.cs
+++ Shooting/Effect.cs
@@ -27,7 +27,7 @@ namespace Shooting
 
         public abstract void Draw(Bitmap canvas);
 
-        static protected void DrawEffect(Bitmap canvas, Bitmap image, Vector2 position, int alpha)
+        static public void DrawEffect(Bitmap canvas, Bitmap image, Vector2 position, int alpha)
         {
             int width = image.Width, height = image.Height;
             var trimmedCanvas = new Bitmap(width, height, PixelFormat.Format32bppArgb);
@@ -85,98 +85,34 @@ namespace Shooting
     {
         float scale = 0;
         int alpha = 100;
-        Element1 magic_circle, ring;
-        Element2[] bits = new Element2[10];
+        EffectElement0 element0A, element0B;
+        EffectElement1[] element1s = new EffectElement1[10];
 
         public Effect1(Vector2 position) : base(position)
         {
             var rand = new Random();
-            magic_circle = new(images[1], position, rand.NextSingle() * MathF.PI, 0.4f);
-            ring = new(images[2], position, 0, 0.6f);
-            for (int i = 0; i < bits.Length; ++i) bits[i] = new(images[3], position);
+            element0A = new(images[1], position, rand.NextSingle() * MathF.PI, 0.4f);
+            element0B = new(images[2], position, 0, 0.6f);
+            for (int i = 0; i < element1s.Length; ++i) element1s[i] = new(images[3], position);
         }
 
         public override void Progress()
         {
-            magic_circle.Progress();
-            ring.Progress();
-            foreach (var bit in bits) bit.Progress();
+            element0A.Progress();
+            element0B.Progress();
+            foreach (var element1 in element1s) element1.Progress();
             if (time >= 5) enable = false;
             ++time;
         }
 
         public override void Draw(Bitmap canvas)
         {
-            magic_circle.Draw(canvas);
-            ring.Draw(canvas);
-            foreach (var bit in bits) bit.Draw(canvas);
+            element0A.Draw(canvas);
+            element0B.Draw(canvas);
+            foreach (var element1 in element1s) element1.Draw(canvas);
         }
 
-        /// <summary>
-        /// 時間と共に膨張し、消えていくエフェクト
-        /// </summary>
-        class Element1
-        {
-            float scale = 0;
-            int alpha = 100;
-            Vector2 position;
-            Bitmap image;
-            float angle;
-            float scaleUpSpeed;
 
-            public Element1(Bitmap image, Vector2 position, float angle, float scaleUpSpeed)
-            {
-                this.position = position;
-                this.image = image;
-                this.angle = angle;
-                this.scaleUpSpeed = scaleUpSpeed;
-            }
-
-            public void Progress()
-            {
-                scale += scaleUpSpeed;
-                alpha -= 5;
-                if (alpha < 0) alpha = 0;
-            }
-
-            public void Draw(Bitmap canvas)
-            {
-                if (scale == 0) return;
-                var r = MathF.Sqrt(MathF.Pow(image.Width / 2.0f, 2) + MathF.Pow(image.Height / 2.0f, 2));
-                var img = new Bitmap((int)(2 * r * scale), (int)(2 * r * scale));
-                var graphics = Graphics.FromImage(img);
-                graphics.Clear(Color.Transparent);
-                float x = image.Width / 2, y = image.Height / 2, c = MathF.Cos(angle), s = MathF.Sin(angle);
-                var vecs = new Vector2[] { new(-x * c + y * s, -x * s - y * c), new(x * c + y * s, x * s - y * c), new(-x * c - y * s, -x * s + y * c) };
-                graphics.DrawImage(image, vecs.Select(v => (PointF)(v * scale + new Vector2(img.Width / 2, img.Height / 2))).ToArray());
-                DrawEffect(canvas, img, new(position.X - img.Width / 2, position.Y - img.Height / 2), alpha);
-            }
-        }
-
-        /// <summary>
-        /// 発生位置からランダムな方向に時間と共に遠ざかり、小さくなっていくエフェクト
-        /// </summary>
-        class Element2
-        {
-            Bitmap image;
-            Vector2 position;
-
-            public Element2(Bitmap image, Vector2 position)
-            {
-                this.image = image;
-                this.position = position;
-            }
-
-            public void Progress()
-            {
-
-            }
-
-            public void Draw(Bitmap canvas)
-            {
-
-            }
-        }
     }
 
     class Effect2 : Effect
@@ -220,6 +156,92 @@ namespace Shooting
         public override void Draw(Bitmap canvas)
         {
 
+        }
+    }
+
+    /// <summary>
+    /// 時間と共に膨張し、消えていくエフェクト要素
+    /// </summary>
+    class EffectElement0
+    {
+        float scale = 0;
+        int alpha = 100;
+        Vector2 position;
+        Bitmap image;
+        float angle;
+        float scaleUpSpeed;
+        const int alpha_down_speed = 5;
+
+        public EffectElement0(Bitmap image, Vector2 position, float angle, float scaleUpSpeed)
+        {
+            this.position = position;
+            this.image = image;
+            this.angle = angle;
+            this.scaleUpSpeed = scaleUpSpeed;
+        }
+
+        public void Progress()
+        {
+            scale += scaleUpSpeed;
+            alpha -= alpha_down_speed;
+            if (alpha < 0) alpha = 0;
+        }
+
+        public void Draw(Bitmap canvas)
+        {
+            if (scale == 0) return;
+            var r = MathF.Sqrt(MathF.Pow(image.Width / 2.0f, 2) + MathF.Pow(image.Height / 2.0f, 2));
+            var img = new Bitmap((int)(2 * r * scale), (int)(2 * r * scale));
+            var graphics = Graphics.FromImage(img);
+            graphics.Clear(Color.Transparent);
+            float x = image.Width / 2, y = image.Height / 2, c = MathF.Cos(angle), s = MathF.Sin(angle);
+            var vecs = new Vector2[] { new(-x * c + y * s, -x * s - y * c), new(x * c + y * s, x * s - y * c), new(-x * c - y * s, -x * s + y * c) };
+            graphics.DrawImage(image, vecs.Select(v => (PointF)(v * scale + new Vector2(img.Width / 2, img.Height / 2))).ToArray());
+            Effect.DrawEffect(canvas, img, new(position.X - img.Width / 2, position.Y - img.Height / 2), alpha);
+        }
+    }
+
+    /// <summary>
+    /// 発生位置からランダムな方向に時間と共に遠ざかり、小さくなっていくエフェクト要素
+    /// </summary>
+    class EffectElement1
+    {
+
+        Bitmap image;
+        Vector2 position;
+        const float max_away_speed = 20;
+        const float max_scale = 2;
+        const float scale_down_speed = 0.06f;
+        const int alpha_down_speed = 10;
+        int alpha = 127;
+        float away_speed;
+        float angle;
+        float scale;
+
+        public EffectElement1(Bitmap image, Vector2 position)
+        {
+            this.image = image;
+            this.position = position;
+            var random = new Random();
+            away_speed = (random.NextSingle() * 0.5f + 0.5f) * max_away_speed;
+            angle = random.NextSingle() * 2.0f * MathF.PI;
+            scale = (random.NextSingle() * 0.5f + 0.5f) * max_scale;
+        }
+
+        public void Progress()
+        {
+            scale -= scale_down_speed;
+            if (scale < 0) scale = 0;
+            alpha -= alpha_down_speed;
+            if (alpha < 0) alpha = 0;
+            position.X += away_speed * MathF.Cos(angle);
+            position.Y += away_speed * MathF.Sin(angle);
+        }
+
+        public void Draw(Bitmap canvas)
+        {
+            if (scale == 0) return;
+            Effect.DrawEffect(canvas, image, new(position.X - image.Width / 2, position.Y - image.Height / 2), alpha);
         }
     }
 }
